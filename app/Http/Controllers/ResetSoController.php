ad5b33186242a8bcc5b21df44610527364717204
Kevin Andhika Ramadhan <kevinandhikaramadhan@gmail.com>
2024-03-05 10:41:45 +0700

    update indomaret sonas

--- app/Http/Controllers/ResetSoController.php
+++ app/Http/Controllers/ResetSoController.php
@@ -91,59 +91,63 @@ class ResetSoController extends Controller
     }
 
     public function datatablesReportAdjustSo(){
+
+        $dt = DB::select("SELECT * FROM TBMASTER_SETTING_SO WHERE MSO_MODIFY_DT in (select max(MSO_MODIFY_DT) from TBMASTER_SETTING_SO)");
+        $TglSO = $dt[0]->mso_tglso;
+        $TglReset = $dt[0]->mso_create_dt;
+
         // dt = QueryOra("SELECT * FROM TBMASTER_SETTING_SO WHERE MSO_MODIFY_DT in (select max(MSO_MODIFY_DT) from TBMASTER_SETTING_SO)")
         // TglSO = Format(dt.Rows(0).Item("mso_tglso"), "dd-MM-yyyy").ToString
         // tglreset = Format(dt.Rows(0).Item("MSO_CREATE_DT"), "dd-MM-yyyy").ToString
 
-        // Str = "select sop_lokasi, sum(total)  as Total "
-        // Str &= " FROM ( "
-        // Str &= "      select "
-        // Str &= "      sop_prdcd, "
-        // Str &= "      sop_lokasi, "
-        // Str &= "      sop_qtyso + coalesce(qty_adj, 0) - sop_qtylpp as qty, "
-        // Str &= "      case when prd_unit = 'KG' then sop_newavgcost / 1000 else sop_newavgcost end as acost,"
-        // Str &= "      sop_qtyso + coalesce(qty_adj, 0) - sop_qtylpp * case when prd_unit = 'KG' then sop_newavgcost / 1000 else sop_newavgcost end as total"
-        // Str &= "    FROM "
-        // Str &= "    ( "
-        // Str &= "      SELECT "
-        // Str &= "      sop_prdcd,sop_lokasi, "
-        // Str &= "      sop_qtyso,sop_qtylpp, "
-        // Str &= "      sop_newavgcost, "
-        // Str &= "      sop_kodeigr, "
-        // Str &= "      sop_tglso "
-        // Str &= "     FROM tbtr_ba_stockopname "
-        // Str &= "    ) as p"
-        // Str &= "    LEFT JOIN "
-        // Str &= "    ("
-        // Str &= "     SELECT adj_kodeigr, adj_tglso, adj_prdcd, adj_lokasi, "
-        // Str &= "     SUM(coalesce(adj_qty, 0)) qty_adj "
-        // Str &= "     FROM tbtr_adjustso "
-        // Str &= "     GROUP BY adj_kodeigr, adj_tglso, adj_prdcd, adj_lokasi "
-        // Str &= "     ) as q ON "
-        // Str &= "         sop_kodeigr = adj_kodeigr "
-        // Str &= "         AND sop_tglso = adj_tglso "
-        // Str &= "         AND sop_prdcd = adj_prdcd "
-        // Str &= "         AND sop_lokasi = adj_lokasi "
-        // Str &= "         AND sop_tglso = to_date('" & Format(TglSO_Eido, "dd-MM-yyyy").ToString & "', 'DD-MM-YYYY')"
-        // Str &= "     LEFT JOIN "
-        // Str &= "     (select prd_unit, prd_prdcd "
-        // Str &= "      from tbmaster_prodmast "
-        // Str &= "      ) as x ON p.sop_prdcd = x.prd_prdcd"
-        // Str &= ") as datas "
-        // Str &= "group by sop_lokasi"
+        $query = '';
+        $query .= "select sop_lokasi, sum(total)  as Total ";
+        $query .= " FROM ( ";
+        $query .= "      select ";
+        $query .= "      sop_prdcd, ";
+        $query .= "      sop_lokasi, ";
+        $query .= "      sop_qtyso + coalesce(qty_adj, 0) - sop_qtylpp as qty, ";
+        $query .= "      case when prd_unit = 'KG' then sop_newavgcost / 1000 else sop_newavgcost end as acost,";
+        $query .= "      sop_qtyso + coalesce(qty_adj, 0) - sop_qtylpp * case when prd_unit = 'KG' then sop_newavgcost / 1000 else sop_newavgcost end as total";
+        $query .= "    FROM ";
+        $query .= "    ( ";
+        $query .= "      SELECT ";
+        $query .= "      sop_prdcd,sop_lokasi, ";
+        $query .= "      sop_qtyso,sop_qtylpp, ";
+        $query .= "      sop_newavgcost, ";
+        $query .= "      sop_kodeigr, ";
+        $query .= "      sop_tglso ";
+        $query .= "     FROM tbtr_ba_stockopname ";
+        $query .= "    ) as p";
+        $query .= "    LEFT JOIN ";
+        $query .= "    (";
+        $query .= "     SELECT adj_kodeigr, adj_tglso, adj_prdcd, adj_lokasi, ";
+        $query .= "     SUM(coalesce(adj_qty, 0)) qty_adj ";
+        $query .= "     FROM tbtr_adjustso ";
+        $query .= "     GROUP BY adj_kodeigr, adj_tglso, adj_prdcd, adj_lokasi ";
+        $query .= "     ) as q ON ";
+        $query .= "         sop_kodeigr = adj_kodeigr ";
+        $query .= "         AND sop_tglso = adj_tglso ";
+        $query .= "         AND sop_prdcd = adj_prdcd ";
+        $query .= "         AND sop_lokasi = adj_lokasi ";
+        $query .= "         AND sop_tglso = to_date('" . $TglSO . "', 'DD-MM-YYYY')";
+        $query .= "     LEFT JOIN ";
+        $query .= "     (select prd_unit, prd_prdcd ";
+        $query .= "      from tbmaster_prodmast ";
+        $query .= "      ) as x ON p.sop_prdcd = x.prd_prdcd";
+        $query .= ") as datas ";
+        $query .= "group by sop_lokasi";
 
-        // brgbaik = Val(dt.Rows(0).Item("Total").ToString)
-        // If dt.Rows.Count < 3 Then
-        //     brgretur = 0
-        //     brgrusak = 0
-        // Else
-        //     brgretur = Val(dt.Rows(1).Item("Total").ToString)
-        //     brgrusak = Val(dt.Rows(2).Item("Total").ToString)
-        // End If
+        $brgBaik = $dt[0]->Total;
+        if(count($dt) < 3){
+            $brgRetur = 0;
+            $brgRusak = 0;
+        }else{
+            $brgRetur = $dt[1]->total;
+            $brgRusak = $dt[2]->total;
+        }
 
-        // Total = brgbaik + brgretur + brgrusak
-
-        // ----
+        $total = (int)$brgBaik + (int)$brgRetur + (int)$brgRusak;
 
         // cmd.CommandText = "SELECT PRS_NAMAPERUSAHAAN, PRS_NAMACABANG FROM TBMASTER_PERUSAHAAN "
         // Da.SelectCommand = cmd

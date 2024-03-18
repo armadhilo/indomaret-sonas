9c1f1e4a1eabf6f478afce7f2e6196eaee98bf3b
Kevin Andhika Ramadhan <kevinandhikaramadhan@gmail.com>
2024-03-18 12:46:22 +0700

    set limit so

--- app/Http/Controllers/SetLimitSoController.php
+++ app/Http/Controllers/SetLimitSoController.php
@@ -41,7 +41,7 @@ class SetLimitSoController extends Controller
         }
 
         $data['tahap'] = $dtCek[0]->mso_flagtahap;
-        $data['tglSO'] = $dtCek[0]->mso_tglso;
+        $data['tglSO'] = Carbon::parse($dtCek[0]->mso_tglso)->format('Y-m-d');
 
         return view('set-limit-so', $data);
     }
@@ -81,6 +81,7 @@ class SetLimitSoController extends Controller
 
     //!! DUMMY DATA
     public function loadDatatables($tahap, $tglSO){
+
         $query = '';
         $query .= "SELECT * FROM ( ";
         $query .= "SELECT DISTINCT PLU, DESKRIPSI, LSO_LOKASI, DIVISI, DEPARTEMENT, KATEGORI, AREAGUDANG, AREATOKO, (AREAGUDANG + AREATOKO) AS TOTAL, LPP, ((AREAGUDANG + AREATOKO  ) - LPP) AS SELISIH, ";
@@ -110,7 +111,7 @@ class SetLimitSoController extends Controller
         $query .= ") DATAS";
         // $query .= " WHERE (((AREAGUDANG + AREATOKO ) - LPP ) * LSO_AVGCOST) <> 0 ORDER BY LSO_LOKASI ASC, NILAI_SELISIH_ABS DESC ";
         $query .= ") DATAS1 ";
-        //! DUMMY
+        //! dummy
         $query .= 'LIMIT 10';
         $data = DB::select($query);
 
@@ -122,7 +123,7 @@ class SetLimitSoController extends Controller
     //! CREATE EXCEL
     public function downloadDataExcel($tahap, $tglSO){
         $query = '';
-        $query .= "SELECT * FROM (";
+        $query .= "SELECT * FROM ( ";
         $query .= "SELECT DISTINCT PLU, DESKRIPSI, LSO_LOKASI, DIVISI, DEPARTEMENT, KATEGORI, AREAGUDANG, AREATOKO, (AREAGUDANG + AREATOKO) AS TOTAL, LPP, ((AREAGUDANG + AREATOKO  ) - LPP) AS SELISIH, ";
         $query .= "ABS(((AREAGUDANG + AREATOKO ) - LPP ) * LSO_AVGCOST) AS NILAI_SELISIH_ABS, ";
         $query .= "(((AREAGUDANG + AREATOKO ) - LPP ) * LSO_AVGCOST) AS NILAI_SELISIH, LSO_FLAGTAHAP, LSO_CREATE_BY ";
@@ -130,22 +131,28 @@ class SetLimitSoController extends Controller
         $query .= "PRD_KODEDIVISI || ' - ' || DIV_NAMADIVISI AS DIVISI, ";
         $query .= "PRD_KODEDEPARTEMENT || ' - ' || DEP_NAMADEPARTEMENT AS DEPARTEMENT, ";
         $query .= "PRD_KODEKATEGORIBARANG || ' - ' || KAT_NAMAKATEGORI AS KATEGORI, ";
-        $query .= "(SELECT coalesce(SUM(LSO_QTY), 0) FROM tbhistory_lhso_sonas WHERE LSO_FLAGTAHAP = '" . $tahap . "' AND LSO_TGLSO = TO_DATE('" . $tglSO . "','YYYY-MM-DD') AND LSO_PRDCD = PRD_PRDCD AND (LSO_KODERAK LIKE 'D%' OR LSO_KODERAK LIKE 'G%') AND LSO_LOKASI = SO.LSO_LOKASI) AS AREAGUDANG, ";
-        $query .= "(SELECT coalesce(SUM(LSO_QTY), 0) FROM tbhistory_lhso_sonas WHERE LSO_FLAGTAHAP = '" . $tahap . "' AND LSO_TGLSO = TO_DATE('" . $tglSO . "','YYYY-MM-DD') AND LSO_PRDCD = PRD_PRDCD AND (LSO_KODERAK NOT LIKE 'D%' AND LSO_KODERAK NOT LIKE 'G%') AND LSO_LOKASI = SO.LSO_LOKASI) AS AREATOKO,  ";
+        // $query .= "(SELECT coalesce(SUM(LSO_QTY), 0) FROM tbhistory_lhso_sonas WHERE LSO_FLAGTAHAP = '" . $tahap . "' AND LSO_TGLSO = TO_DATE('" . $tglSO . "','YYYY-MM-DD') AND LSO_PRDCD = PRD_PRDCD AND (LSO_KODERAK LIKE 'D%' OR LSO_KODERAK LIKE 'G%') AND LSO_LOKASI = SO.LSO_LOKASI) AS AREAGUDANG, ";
+        // $query .= "(SELECT coalesce(SUM(LSO_QTY), 0) FROM tbhistory_lhso_sonas WHERE LSO_FLAGTAHAP = '" . $tahap . "' AND LSO_TGLSO = TO_DATE('" . $tglSO . "','YYYY-MM-DD') AND LSO_PRDCD = PRD_PRDCD AND (LSO_KODERAK NOT LIKE 'D%' AND LSO_KODERAK NOT LIKE 'G%') AND LSO_LOKASI = SO.LSO_LOKASI) AS AREATOKO,  ";
+        //! dummy
+        $query .= "0 AS AREAGUDANG, ";
+        $query .= "0 AS AREATOKO,  ";
+
         $query .= "(LSO_ST_SALDOAKHIR) AS LPP, LSO_LOKASI, LSO_FLAGTAHAP, LSO_CREATE_BY, LSO_AVGCOST ";
         $query .= "FROM TBMASTER_PRODMAST, tbhistory_lhso_sonas SO, tbmaster_divisi, tbmaster_departement, tbmaster_kategori ";
-        $query .= "WHERE LSO_TGLSO = TO_DATE('" . $tglSO . "','YYYY-MM-DD') ";
-        $query .= "AND LSO_LOKASI = '01' ";
+        $query .= "WHERE LSO_LOKASI = '01' ";
+        // $query .= "AND LSO_TGLSO = TO_DATE('" . $tglSO . "','YYYY-MM-DD') ";
         $query .= "AND LSO_PRDCD = PRD_PRDCD ";
-        $query .= "AND LSO_FLAGTAHAP = '" . $tahap . "' ";
+        // $query .= "AND LSO_FLAGTAHAP = '" . $tahap . "' ";
         $query .= "and prd_kodedivisi = div_kodedivisi ";
         $query .= "and prd_kodedivisi = dep_kodedivisi ";
         $query .= "and prd_kodedepartement = dep_kodedepartement ";
         $query .= "and prd_kodedepartement = kat_kodedepartement ";
         $query .= "and prd_kodekategoribarang = kat_kodekategori ";
-        $query .= ")AS DATAS ";
-        $query .= " WHERE (((AREAGUDANG + AREATOKO ) - LPP ) * LSO_AVGCOST) <> 0 ORDER BY LSO_LOKASI, DIVISI ASC, NILAI_SELISIH_ABS DESC ";
-        $query .= ")AS DATAS1";
+        $query .= ") DATAS";
+        // $query .= " WHERE (((AREAGUDANG + AREATOKO ) - LPP ) * LSO_AVGCOST) <> 0 ORDER BY LSO_LOKASI ASC, NILAI_SELISIH_ABS DESC ";
+        $query .= ") DATAS1 ";
+        //! dummy
+        $query .= 'LIMIT 10';
 
         $data['data'] = DB::select($query);
 
@@ -168,7 +175,7 @@ class SetLimitSoController extends Controller
         $import = new SetLimitSoImport();
         Excel::import($import, $file);
         $data = $import->data;
-        // return $data;
+
         //! LOOP DATA DATA DARI EXCEL
         $array = [];
         foreach($data as $item){
@@ -176,7 +183,6 @@ class SetLimitSoController extends Controller
                 $array[] = $item["plu"] .'-'. $item["lokasi"];
             }
         }
-
         //! END LOOP DATA
 
         DB::table('tbtr_lokasi_so')
